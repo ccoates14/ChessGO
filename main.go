2617c3709916b2d68bd45bb879fbe4c4ebde0bd3
chandler coates <chandlercoates@chandlers-MacBook-Pro.local>
2024-10-27 15:51:08 -0600

    setting up proj

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(`          _             _       _    _           _           _                  _          _                    _              _          _     
+        /\ \           / /\    / /\ /\ \        / /\        / /\               /\ \       /\ \     _           /\ \           /\ \       /\_\   
+       /  \ \         / / /   / / //  \ \      / /  \      / /  \              \ \ \     /  \ \   /\_\        /  \ \         /  \ \     / / /   
+      / /\ \ \       / /_/   / / // /\ \ \    / / /\ \__  / / /\ \__           /\ \_\   / /\ \ \_/ / /       / /\ \_\       / /\ \ \   / / /_   
+     / / /\ \ \     / /\ \__/ / // / /\ \_\  / / /\ \___\/ / /\ \___\         / /\/_/  / / /\ \___/ /       / / /\/_/      / / /\ \ \ / /___/\  
+    / / /  \ \_\   / /\ \___\/ // /_/_ \/_/  \ \ \ \/___/\ \ \ \/___/        / / /    / / /  \/____/       / / / ______   / / /  \ \_\\____ \ \ 
+   / / /    \/_/  / / /\/___/ // /____/\      \ \ \       \ \ \             / / /    / / /    / / /       / / / /\_____\ / / /   / / /    / / / 
+  / / /          / / /   / / // /\____\/  _    \ \ \  _    \ \ \           / / /    / / /    / / /       / / /  \/____ // / /   / / /    / / /  
+ / / /________  / / /   / / // / /______ /_/\__/ / / /_/\__/ / /       ___/ / /__  / / /    / / /       / / /_____/ / // / /___/ / /    _\/_/   
+/ / /_________\/ / /   / / // / /_______\\ \/___/ /  \ \/___/ /       /\__\/_/___\/ / /    / / /       / / /______\/ // / /____\/ /    /\_\     
+\/____________/\/_/    \/_/ \/__________/ \_____\/    \_____\/        \/_________/\/_/     \/_/        \/___________/ \/_________/     \/_/     
+                                                                                                                                                `)
+
+	//
+	// This will be the complete game of Chess, including two player more and a really basic AI to play against. 
+	// The first version will be purely in cmd and depending on how long this takes me I will come back around later and add an actual UI.
+	// The default will be via cmd but a flag will later be added to launch with UI.
+	// 
+
+	// TODO:
+
+	// 1. Build basic game loop with IO
+	// 2. 
+
+	// structure
+
+	// main
+	//	game loop
+
+	// player
+
+	// ai 
+
+	// board
+	
+	// piece
+}
